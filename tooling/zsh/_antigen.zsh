7c19f6510795045f9a60a9660a8b0b1b00542bc0
sebastian <mail@srwip.com>
2022-01-11 13:24:24 +0100

    latest changes

--- tooling/zsh/_antigen.zsh
+++ tooling/zsh/_antigen.zsh
@@ -34,7 +34,6 @@ antigen bundle nvm
 
 antigen theme romkatv/powerlevel10k
 
-
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f $HOME/.p10k.zsh ]] || source $HOME/.p10k.zsh
 
